090ff4ca9eecd4f73cbbecf0c0975ae274f53c60
Rosângela Nery <rosangela2nery@gmail.com>
2022-09-30 15:22:46 -0300

    Updating Database

--- src/controllers/categoriesControllers.js
+++ src/controllers/categoriesControllers.js
@@ -1,17 +1,24 @@
 import { status_code } from '../enums/status.js';
 import { connection } from '../pg/database.js';
 
+async function categoriesPost(req, res) {
+    try {
+        
+    } catch (error) {
+        res.status(status_code.server_error).send(error.message);
+    }
+}
+
 async function categoriesGet(req, res) {
 
     try {
         const categories = await connection.query('SELECT * FROM categories;');
 
-        console.log(categories.rows);
+        console.log(categories);
+        res.send(categories.rows);
     } catch (error) {
         res.status(status_code.server_error).send(error.message);
     }
-
-    res.send(status_code.ok);
 }
 
 export { categoriesGet };
--- src/pg/database.js
+++ src/pg/database.js
@@ -3,19 +3,9 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connectionString = process.env.DATABASE_URL;
-const host = 'localhost';
-const port = 5432;
-const user = 'postgres';
-const password = '123456';
-const database = 'boardcamp';
 
 const connection = new Pool({
     connectionString,
-    host,
-    port,
-    user,
-    password,
-    database,
 });
 
 export { connection };
