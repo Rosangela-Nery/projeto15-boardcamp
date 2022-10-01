cb7942b58a65ad7f4a7029953fdd9fe6ec3c5e2c
Rosângela Nery <rosangela2nery@gmail.com>
2022-10-01 19:35:20 -0300

    Validation categories with joi

--- src/controllers/categoriesControllers.js
+++ src/controllers/categoriesControllers.js
@@ -1,9 +1,19 @@
 import { status_code } from '../enums/status.js';
 import { connection } from '../pg/database.js';
+import { categoriesSchema } from '../schemas/validationSchemas.js';
 
 async function categoriesPost(req, res) {
 
     const { name } = req.body;
+
+    const validation = categoriesSchema.validate(name , {
+        abortEarly: false,
+    });
+
+    if(validation.error) {
+        res.status(status_code.bad_request).send({"message": "É necessário preencher todos os campos!"});
+        return;
+    }
 
     try {
         connection.query("INSERT INTO categories (name) VALUES ($1);", [name]);
--- src/schemas/validationSchemas.js
+++ src/schemas/validationSchemas.js
@@ -1,2 +1,9 @@
 import joi from 'joi';
 
+const categoriesSchema = joi.object({
+    name: joi.string().min(2).required(),
+});
+
+export {
+    categoriesSchema
+}
